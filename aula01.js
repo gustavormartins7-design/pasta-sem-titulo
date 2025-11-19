1d225b5471ba7d3065fbea7eb10f459aa930d2d5
GUSTAVO ROCHA MARTINS <49124646865@imaccommand013.local>
2025-11-19 11:06:30 -0300

    Aula 01

---
+++ aula01.js
@@ -0,0 +1,66 @@
+// window.alert{("minha primeira mensagem!")
+//     }
+//     window.confirm{("hoje e sexta?")}
+
+// comentrio de linha
+/*
+comentario varias linhas
+*/
+      
+    // let nome=window.prompt("qual e o seu nome?")
+
+    //     window alert("bem vindo (a)! " +nome) 
+    //     console.log("apresenta mensagem na tela")
+
+    // let numero01 = parselnt(prompt("digite o primeiro numero:"))
+    
+    // prompt ("digite o primeiro numero")
+
+    // let numero02 =parseInt (prompt("digite o segundo numero:"))
+    
+    // prompt ("digite o segundo numero")
+
+    // let soma = numero01 + numero02
+
+    // console.log(soma)
+
+    // let num01 = parseFloat(prompt ("digite algum momento"))
+
+    // let num02 = parseFloat(prompt ("digite algum numero:"))
+
+    // let resultado = num01 + num02
+
+    //  console.log (resultado)
+
+    // let idade = parseInt(prompt("qual e sua idade?"))
+    // if ( idade >= 18) {
+    //     console. log ("voce e maior de idade da - lhee")
+    // }
+    // else{
+    //     console.log("voce e menor de idade, baixinho")
+    // }
+
+    let estacoes= prompt("digite uma estaçao do ano")
+
+    switch (estacoes) {
+        case "verao":
+           console, log("muito calor e praia") 
+    
+            break;
+           case "outono":
+           console.log ("as folhas caem no final")
+           break;
+
+           case "Inverno":
+            console.log("Está muito frio")
+            break;
+
+           case"primavera":
+           console.log("eu vejo flores em Voce...")
+           break;
+
+           
+        default:
+            console.log ("Nao existe estaçao!!!")
+            break;
+    }
